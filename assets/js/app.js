58f54bcff6b6b7d8f5b9859ab6f578aef0c77157
narozbekjon <jamilovnavroz32@gmail.com>
2024-02-02 09:07:48 +0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+alert("Salom foydalanuvchi siz Jamilov Navro'zbekning siteiga kirmoqchimisiz")
+var isim = prompt("Ismingizni kiriting")
+var familya = prompt("Familyangizni kiriting")
+var yosh = prompt("Yoshngizni kiriting")
+console.log("Salom menig ismim " + isim + " Familyam" + familya + " Yoshim" + yosh + "da");
+confirm("Tabriklaymiz foydalanuvchi siz Jamilov Navro'zbekning siteiga muaffaqqiyatli tarizda kirdingiz")
